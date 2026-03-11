6e1fae3d4d5a98033d4206711de58e79022c7248
BaoHan1712 <hanquocbao1712@gmail.com>
2026-03-11 09:31:29 +0700

    main

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import uuid
 import json
+import os
 
 app = Flask(__name__)
 
@@ -69,5 +70,6 @@ def checkout():
         "total": total_amount
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
